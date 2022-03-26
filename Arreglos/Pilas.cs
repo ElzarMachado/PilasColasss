81a1c108a6dbc10d4a0e9e491623b8ca494c773d
ElzarMachado <101478824+ElzarMachado@users.noreply.github.com>
2022-03-26 12:31:20 -0700

    act clase

--- Arreglos/Pilas.cs
+++ Arreglos/Pilas.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Arreglos
 {
-    internal class Pilas
+    public class Pilas
     {
+        private string[] array;
+        private int max;
+        private int tope;
+
+        public Pilas(int tamano)
+        {
+            array = new string[tamano];
+            max = array.Length - 1;
+            tope = 0;
+
+        }
+        private bool ValidaVacio()
+        {
+            //    if (tope < 1)
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            return (tope < 1);
+        }
+
+        private bool ValidaLleno()
+        {
+            return (tope > max );
+        }
     }
 }
